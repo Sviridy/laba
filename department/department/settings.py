5ba456093876c06e154fad3c6f1ed200c7f20b85
Sviridy <john591998@mail.ru>
2021-10-08 20:26:54 +0300

    test

--- department/department/settings.py
+++ department/department/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y3$@bf^j$7#ce$7hg$s%a%3(^*69f4b)^fzm$u6!&r2fk@!tjt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
