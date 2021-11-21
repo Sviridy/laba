4717fb1ec2573c785a84a5fecc047f1198df2393
Sviridy <john591998@mail.ru>
2021-11-22 00:50:05 +0300

    bd3

--- department/department_app/views/__init__.py
+++ department/department_app/views/__init__.py
@@ -1,7 +1,7 @@
 """Views"""
-import logging
-logger = logging.getLogger(__name__)
-logger.debug('REST_Framework')
+# import logging
+# logger = logging.getLogger(__name__)
+# logger.debug('REST_Framework')
 from department_app.views.department_views import DepartmentCreateView, DepartmentListView, DepartmentDetailView
 from department_app.views.employee_views import EmployeeCreateView, EmployeeListView, EmployeeDetailView
 from department_app.views.specialization_views import SpecializationCreateView, SpecializationListView, \
