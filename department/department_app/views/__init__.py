24feb6bbfb7242cdc11949ef6597e47c828c928c
Sviridy <john591998@mail.ru>
2022-02-20 17:34:45 +0300

    test_3

--- department/department_app/views/__init__.py
+++ department/department_app/views/__init__.py
@@ -1,11 +1,5 @@
 """Views"""
 from department_app.views.department_views import DepartmentViewSet
-from department_app.views.department_app_views import DepartmentHome, AddDepartment, EditDepartment, delete_department
 from department_app.views.employee_views import EmployeeViewSet
-from department_app.views.employee_app_views import EmployeeHome, AddEmployee, EditEmployee, delete_employee, \
-    EmployeeSearch
 from department_app.views.specialization_views import SpecializationViewSet
-from department_app.views.specialization_app_views import SpecializationHome, EditSpecialization, AddSpecialization, \
-    delete_specialization
 from department_app.views.positions_views import PositionsViewSet
-from department_app.views.positions_app_views import PositionsHome, EditPositions, AddPositions, delete
