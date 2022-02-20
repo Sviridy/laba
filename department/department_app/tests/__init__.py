68bdc1fabfd28b1031ed97c1950ffa7e65749f23
Sviridy <john591998@mail.ru>
2022-02-20 17:16:47 +0300

    test

--- department/department_app/tests/__init__.py
+++ department/department_app/tests/__init__.py
@@ -1,3 +1,3 @@
 """Init tests"""
 from department_app.tests.test_modul import TestModels
-from department_app.tests.test_rest import DepartmentTests
+from department_app.tests.test_rest import CreateTests, ReadTests, DeleteTests, UpdateTests
